e95e71d9dc48b554b2b1d320363b7dee7cc5ca96
MAGGI4\sake <sergioscattone>
2019-07-07 19:10:39 -0300

    front

---
+++ front/src/_helpers/auth-header.js
@@ -0,0 +1,10 @@
+export function authHeader() {
+    // change to user bearer token 
+    let user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.authdata) {
+        return { 'Authorization': 'Basic ' + user.authdata };
+    } else {
+        return {};
+    }
+}
